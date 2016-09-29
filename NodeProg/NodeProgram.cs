b8b03b30a32f04208f72c31fdf1b23e5a0e99e9d
yuriyoleynikov <yuriyoleynikov.ru@gmail.com>
2016-09-29 16:44:11 +0300

    Test

--- NodeProg/NodeProgram.cs
+++ NodeProg/NodeProgram.cs
@@ -1,9 +1,16 @@
-﻿namespace NodeProg
+﻿using System;
+
+namespace NodeProg
 {
     class NodeProgram
     {
         static void Main(string[] args)
         {
+            var ln = Node.ParseNodes("test,(test2)");
+            foreach (var node in ln)
+            {
+                Console.Write(node);
+            }
         }
     }
 }
